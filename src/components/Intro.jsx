d88e58c5d1ee424703e1702cdffe969deaf4b0d5
Nour Hamdy <80398256+nour-17@users.noreply.github.com>
2022-08-29 11:47:03 +0200

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,7 @@ export default function Intro({ startQuiz }) {
   return (
     <div className="intro">
       <h1 className="intro__title">Quizzical</h1>
-      <p>How much do you know?</p>
+      <p>test your general knowledge around a variety of subjects.</p>
       <button className="submit intro-btn" onClick={startQuiz}>
         Start
       </button>
